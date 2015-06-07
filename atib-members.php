e3d9a571bdc5e0db885621ee141a726c322c20b7
Fredrik Nilsson <fn@live.se>
2015-06-07 19:55:10 +0200

    Move of ATIB member functions from theme-file to plugin. CPT seems to work OK. Ouput don't work on some pages ( headers already sent by ...atib-members.php:1).

---
+++ atib-members.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: ATIB Members
+Plugin URI:  https://github.com/fnlive/ATIB-Shortcodes
+Description: Membership site functions for "Annika och Torkel i Berg"
+Version:     0.0.1
+Author:      Fredrik Nilsson
+Author URI:  https://github.com/fnlive
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
